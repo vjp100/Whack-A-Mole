f2872a29c829499f8b5eae8bec99bd86636814f5
vjp100 <vishalpowell@gmail.com>
2026-06-01 17:39:23 -0400

    Should be working. Could possible sample in the middle of data instead of at edge (i.e. clk_cnt ~= 18 or something but should be fine).

--- README.md
+++ README.md
@@ -1,18 +1,12 @@
 # Whack-A-Mole
 Engs 31 final project. Team members: Alisha Qureshi, Mazvita Nhidza, Vishal Powell 
 
-## Update 1/6/23 : 1:46am
-
-Haven't finished coding Joystick module. Need to include registers for x and y (with the parsing of the data to take the 10 bit entry that's split across 16 bits). Need to do math, thinking of potentially having an x shift register (maybe x1, x2) and then anding them into.
-
-Actually i'll have a generic shift register that takes the whole thing maybe (?? all 40 bytes) then i shift them in to the appropriate space? not sure tbh.
-
-anyways either shift specifically to the registers then format them into only one.
-* then check if they fit the criteria for left, right, up or down and provide a monopulsed signal for it.
-* Idk same for buttons--parse and see if button 1 (start/stop) or 2 (reset) is pressed.
+## Update 1/6/26 : 5:34 PM
+Lowk should be close to being done. need to make a testbench to work with the file but it's solid? idk will see. 
 
 ### To DO:
 #### REMOVE `PCLK_CNRT` or `PCLK` and use normal clk for VGA. We're generating a 25mhz clock instead of dividing.
+#### NEED to handle MOSI (i think? might be MISO) in top level by tieing them to ground. Also LED bits i think
 * ~I think I'll either finish debouncing and working on joystick logic tonight (probaly not gonna happen lowk) or tomorrow morning. It's SPI and idr how to do that lol.~
 
 * ADD SYSTEM_CLOCK_GENERATOR and tick_generator to vivado project and instanciate the different clocks...?
@@ -21,6 +15,15 @@ anyways either shift specifically to the registers then format them into only on
 
 Old Updates:
 ---
+## Update 1/6/26 : 1:46am
+
+Haven't finished coding Joystick module. Need to include registers for x and y (with the parsing of the data to take the 10 bit entry that's split across 16 bits). Need to do math, thinking of potentially having an x shift register (maybe x1, x2) and then anding them into.
+
+Actually i'll have a generic shift register that takes the whole thing maybe (?? all 40 bytes) then i shift them in to the appropriate space? not sure tbh.
+
+anyways either shift specifically to the registers then format them into only one.
+* ~then check if they fit the criteria for left, right, up or down and provide a monopulsed signal for it.~
+* ~Idk same for buttons--parse and see if button 1 (start/stop) or 2 (whacked) is pressed.~
 
 ## Update 30/5/26 : 12:22am
 
--- joystick_module.vhd
+++ joystick_module.vhd
@@ -19,8 +19,8 @@ use UNISIM.Vcomponents.ALL;
 --Entitity Declarations
 --=============================================================
 entity JoyStick is
-	generic(
-		N_SHIFTS     				: integer);
+	-- generic(
+	-- 	N_SHIFTS     				: integer);
 	port(
 	    --25 MHz serial clock
 		clk_port    				: in  std_logic;	
@@ -37,8 +37,10 @@ entity JoyStick is
         left_move                  : out std_logic;
         up                         : out std_logic;
         down                       : out std_logic;
-        spi_sclk                   : out std_logic;
-        button_port                : out std_logic_vector(2 downto 0));
+        spi_sclk_port              : out std_logic;
+        button_port                : out std_logic_vector(2 downto 0);
+        reset_button               : out std_logic;
+        whack_button               : out std_logic);
 end JoyStick; 
 
 --=============================================================
@@ -54,31 +56,33 @@ signal take_sample          : std_logic := '0';
 -- signal shift_x2_enable		: std_logic := '0';
 -- signal shift_y2_enable		: std_logic := '0';
 -- signal shift_buttons_enable	: std_logic := '0';
-signal load_enable		    : std_logic := '0';
-signal delay_enable		    : std_logic := '0';
-signal shift_reg	        : std_logic_vector(39 downto 0) := (others => '0');
 -- signal x_1_reg              : std_logic_vector(7 downto 0) := (others => '0');
 -- signal y_1_reg              : std_logic_vector(7 downto 0) := (others => '0');
 -- signal x_2_reg              : std_logic_vector(7 downto 0) := (others => '0');
 -- signal y_2_reg              : std_logic_vector(7 downto 0) := (others => '0');
 -- signal temp_buttons_reg     : std_logic_vector(7 downto 0) := (others => '0');
+signal shift_enable		    : std_logic := '0';
+signal load_enable		    : std_logic := '0';
+signal delay_enable		    : std_logic := '0';
+signal shift_reg	        : std_logic_vector(39 downto 0) := (others => '0');
 signal x_axis_reg           : std_logic_vector(9 downto 0) := (others => '0');
 signal y_axis_reg           : std_logic_vector(9 downto 0) := (others => '0');
 signal button_reg           : std_logic_vector(2 downto 0) := (others => '0');
 signal clk_divider_cntr     : integer := 0;
-signal clk_divider_tc   : std_logic := '0';	-- terminal count for clock divider
-signal delay_cntr    : integer := 0;
-signal delay_tc      : std_logic := '0';	-- terminal count for SS delay
-signal bit_count		: integer := 0;
-signal bit_tc			: std_logic := '0';	-- terminal count for bit counter
-signal shift_cntr      : integer := 0;
-signal shift_tc       : std_logic := '0';	-- terminal count for number of shifts
-signal spi_sclk       : std_logic := '0';
+signal clk_divider_tc       : std_logic := '0';	-- terminal count for clock divider
+signal delay_cntr           : integer := 0;
+signal delay_tc             : std_logic := '0';	-- terminal count for SS delay
+signal bit_count		    : integer := 0;
+signal bit_tc			    : std_logic := '0';	-- terminal count for bit counter
+signal shift_cntr           : integer := 0;
+signal N_SHIFT_TC           : std_logic := '0';	-- terminal count for number of shifts
+signal spi_sclk             : std_logic := '0';
 
-CONSTANT
-    DELAY_COUNT : integer := 375; -- 15us delay at 25MHz clock per spec
-    N_BITS : integer := 8; -- Number of bits to shift in
-    N_SHIFTS : integer := 5; -- Number of shifts to perform (5 bytes)
+CONSTANT DELAY_COUNT : integer := 375; -- 15us delay at 25MHz clock per spec
+CONSTANT N_BITS      : integer := 8; -- Number of bits to shift in
+CONSTANT N_SHIFTS    : integer := 5; -- Number of shifts to perform (5 bytes)
+
+
 type state_type is (IDLE, SHIFT, DONE, SS_DELAY, DELAY, PARSE);
 signal current_state, next_state : state_type := IDLE;
 
@@ -104,16 +108,24 @@ end process clk_divider;
 SCLK_generator: process(clk_port)
 begin
     if rising_edge(clk_port) then
-        if clk_divider_tc = '1' then
-            spi_sclk <= not spi_sclk;
+        if shift_enable = '1' then
+            if clk_divider_cntr + 1 < 12 then
+                spi_sclk <= '0';
+            else
+                spi_sclk <= '1';
+            end if;
+        else
+            spi_sclk <= '0';
         end if;
     end if;
 end process SCLK_generator;
 
-take_sample_sync: process(clk_port)
+take_sample_sync: process(clk_port,take_sample_port,clk_divider_tc)
 begin
     if rising_edge(clk_port) then
-        take_sample <= take_sample_port or take_sample;
+        if clk_divider_tc = '1' then
+            take_sample <= take_sample_port;
+        end if;
     end if;
 end process take_sample_sync;
 
@@ -122,7 +134,7 @@ end process take_sample_sync;
 --State Update:
 --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++		
 
-state_update: process(clk_port)
+state_update: process(clk_port,clk_divider_tc)
 begin
     if rising_edge(clk_port) then
         if clk_divider_tc = '1' then --idk how to make this work with the clock divider external so im doing it here.
@@ -135,7 +147,7 @@ end process state_update;
 --Next State Logic:
 --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-next_state_process: process(current_state, take_sample, bit_tc, delay_tc)
+next_state_process: process(current_state, take_sample, bit_tc, delay_tc,N_SHIFT_TC)
 begin
     next_state <= current_state;	-- default state is to hold
     case current_state is
@@ -181,11 +193,13 @@ begin
             spi_cs_port <= '1';		-- active low
         when SS_DELAY =>
             delay_enable <= '1';
+            spi_cs_port <= '0';		-- active low
         when SHIFT =>
             shift_enable <= '1';
             spi_cs_port <= '0';		-- active low
         when DELAY =>
             delay_enable <= '1';
+            spi_cs_port <= '0';		-- active low
         when PARSE =>
             load_enable <= '1';
         when DONE =>
@@ -198,7 +212,7 @@ end process Output_process;
 --Timer Sub-routines:
 --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-fifteen_us_delay: process(clk_port, delay_enable, clk_divider_tc)
+fifteen_us_delay: process(clk_port, delay_enable, clk_divider_tc,delay_cntr)
 begin
     -- Implementation for 15 microsecond delay
     if rising_edge(clk_port) then
@@ -227,26 +241,25 @@ shift_register: process(clk_port,shift_enable,bit_tc,clk_divider_tc)
 begin
     if rising_edge(clk_port) then
         if clk_divider_tc = '1' then
-            if shift_enable = '1'  and bit_tc = '0' then
+            if shift_enable = '1' then
                 shift_reg <= shift_reg(38 downto 0) & spi_s_data_port;
                 if bit_count + 1 = N_BITS then
                     bit_count <= 0;
-                    bit_tc <= '1';
+                    if shift_cntr + 1 = N_SHIFTS then
+                        shift_cntr <= 0;
+                    else
+                        shift_cntr <= shift_cntr + 1;
+                     end if;
                 else
                     bit_count <= bit_count + 1;
-                    bit_tc <= '0';
-                end if;
-                if shift_cntr + 1 = N_SHIFTS then
-                    shift_cntr <= 0;
-                    shift_tc <= '1';
-                else
-                    shift_cntr <= shift_cntr + 1;
-                    shift_tc <= '0';
                 end if;
             end if;
         end if;
     end if;
 end process shift_register;
+
+bit_tc<= '1' when bit_count = N_BITS-1 else '0';
+N_SHIFT_TC <= '1' when shift_cntr = N_SHIFTS-1 else '0';
 
 Parse_process: process(clk_port,load_enable,clk_divider_tc)
 begin
@@ -254,21 +267,26 @@ begin
         if clk_divider_tc = '1' then
             if load_enable = '1' then
                 x_axis_reg <= shift_reg(25 downto 24) & shift_reg(39 downto 32); -- concatenating the two nibbles for x.
-                y_axis_reg <= shift_reg(23 downto 16) & (shift_reg(9 downto 8)); -- concatenating the two nibbles for y.
+                y_axis_reg <= shift_reg(9 downto 8) & (shift_reg(23 downto 16)); -- concatenating the two nibbles for y.
                 button_reg <= shift_reg(2 downto 0); -- the last 3 bits are the buttons and Joystick moved.
             end if;
         end if;
     end if;
 end process Parse_process;
 
-Handle_outputs: process(x_axis_reg, y_axis_reg, button_reg)
-begin
+--==========================================
+--          Handle_outputs
+--===========================================
+
     x_axis_port <= x_axis_reg;
     y_axis_port <= y_axis_reg;
     button_port <= button_reg;
-    right_move <= '1' when x_axis_reg > "1000000000" else '0'; -- if x is greater than 512, we're moving right.
-    left_move <= '1' when x_axis_reg < "0111111111" else '0'; -- if x is less than 511, we're moving left.
-    up <= '1' when y_axis_reg > "1000000000" else '0'; -- if y is greater than 512, we're moving up.
-    down <= '1' when y_axis_reg < "0111111111" else '0'; -- if y is less than 511, we're moving down.
-end process Handle_outputs;
+    right_move <= '1' when unsigned(x_axis_reg) > unsigned("1001100100") else '0'; -- if x is greater than 612, we're moving right. => implies 100 deadzone (~7.5% of 1023) on either side of the center value of 512.
+    left_move <= '1' when unsigned(x_axis_reg) < unsigned("0110011100") else '0'; -- if x is less than 412, we're moving left.
+    up <= '1' when unsigned(y_axis_reg) > unsigned("1001100100") else '0'; -- if y is greater than 612, we're moving up.
+    down <= '1' when unsigned(y_axis_reg) < unsigned("0110011100") else '0'; -- if y is less than 412, we're moving down.
+    reset_button <= '1' when button_reg(1) = '1' else '0'; -- if the first button bit is 1, reset button is pressed.
+    whack_button <= '1' when button_reg(2) = '1' else '0'; -- if the second button bit is 1, start/stop button is pressed.
+
+    spi_sclk_port <= spi_sclk; -- output the generated SCLK to the port.
 end Behavioral; 
