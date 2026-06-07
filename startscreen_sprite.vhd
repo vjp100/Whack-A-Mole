187145aac74675f52739ac08f7d36f8548e682c1
Mazvita Nhidza <mazvita.nhidza.28@dartmouth.edu>
2026-06-07 17:04:35 -0400

    Add files via upload

---
+++ startscreen_sprite.vhd
@@ -0,0 +1,46 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity startscreen_sprite is
+    port (
+        clk   : in  std_logic;
+        row   : in  integer range 0 to 19;
+        col   : in  integer range 0 to 159;
+        color : out std_logic_vector(11 downto 0)
+    );
+end startscreen_sprite;
+
+
+architecture Behavioral of startscreen_sprite is
+
+    signal rom_addr : std_logic_vector(11 downto 0);
+    signal rom_data : std_logic_vector(11 downto 0);
+    
+    component startscreen_rom
+    port(
+        clka : IN STD_LOGIC;
+        ena : IN STD_LOGIC;
+        addra : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
+        douta : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)
+    );
+  end component;
+
+begin
+
+    -- Convert row/col into ROM address
+    rom_addr <= std_logic_vector(to_unsigned(row * 160 + col,12));
+
+    -- Instantiate Block Memory Generator IP
+    u_startscreen_sprite : startscreen_rom
+        port map (
+            clka  => clk,
+            ena   =>  std_logic'('1'),
+            addra => rom_addr,
+            douta => rom_data
+        );
+
+    -- Output color from ROM
+    color <= rom_data;
+
+end Behavioral;
